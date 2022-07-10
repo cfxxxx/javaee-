5090aaf9d46261a1cd9eea3ce416413a206b9257
程风夏 <102495157+cfxxxx@users.noreply.github.com>
2022-07-10 22:55:15 +0800

    Delete Spring5_demo02 directory

--- Spring5_demo02/src/com/spring5/autowire/Dept.java
+++
@@ -1,8 +0,0 @@
-package com.spring5.autowire;
-
-public class Dept {
-    @Override
-    public String toString() {
-        return "Dept{}";
-    }
-}
--- Spring5_demo02/src/com/spring5/collectiontype/Subject.java
+++
@@ -1,9 +0,0 @@
-package com.spring5.collectiontype;
-
-public class Subject {
-    private String name;//课程名称
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
